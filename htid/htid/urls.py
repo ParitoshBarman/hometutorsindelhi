4de7240ed1cd52c0504ea512a26ce2d0e63c0c51
ParitoshBarman <barmanpari163@gmail.com>
2024-08-24 17:01:48 +0530

    Admin panel name change

--- htid/htid/urls.py
+++ htid/htid/urls.py
@@ -19,6 +19,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.site_header = "Home Tutors in Delhi Admin"
+admin.site.site_title = "Home Tutors in Delhi Admin Portal"
+admin.site.index_title = "Welcome to Home Tutors in Delhi Portal"
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', include('htidapp.urls'))
